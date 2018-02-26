a3ec404cf75a18f2f27a656ff54282a7e4849d6d
mrswimmer <mrswimmerlab@gmail.com>
2018-02-26 22:55:14 +0700

    fix

--- app/src/main/java/com/membattle/domain/service/Service.java
+++ app/src/main/java/com/membattle/domain/service/Service.java
@@ -1,19 +1,11 @@
 package com.membattle.domain.service;
 
-
-import android.content.Context;
-import android.widget.ImageView;
-
-import com.membattle.R;
 import com.membattle.data.api.API;
 import com.membattle.data.api.req.Id;
 import com.membattle.data.api.req.RegistrationUser;
 import com.membattle.data.api.req.Secret;
 import com.membattle.data.api.res.Exres;
 import com.membattle.data.api.res.rate.Rate;
-import com.squareup.picasso.Picasso;
-
-import rx.Observable;
 import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -61,36 +53,5 @@ public class Service {
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(callback::onSuccess, callback::onError);
-    }
-
-    public Subscription loadMem(String url, ImageView imageView, Context context) {
-        return Observable.just(url)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(s -> Picasso.with(context)
-                        .load(url)
-                        .placeholder(R.color.white)
-                        .error(R.color.white)
-                        .into(imageView)
-                        /*new Observer<String>() {
-                    @Override
-                    public void onCompleted() {
-                        Picasso.with(context)
-                                .load(url)
-                                .placeholder(R.color.white)
-                                .error(R.color.white)
-                                .into(imageView);
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(String s) {
-
-                    }
-                }*/);
     }
 }
---
+++ app/src/main/java/com/membattle/presentation/game/fragment/GameFragmentView.java
@@ -0,0 +1,11 @@
+package com.membattle.presentation.game.fragment;
+
+import com.arellomobile.mvp.MvpView;
+
+interface GameFragmentView extends MvpView {
+    void showErrorToast(String error);
+
+    void setMemes(String urlTop, String urlBottom, boolean firstPair);
+
+    void showResult(String topLikes, String bottomLikes, String topStatus, String bottomStatus);
+}
