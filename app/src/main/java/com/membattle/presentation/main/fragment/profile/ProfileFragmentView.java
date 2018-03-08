8dea6ffd2bcea169f58efcfed0521850a32ccdaf
mrswimmer <mrswimmerlab@gmail.com>
2018-03-08 12:36:24 +0700

    update gradle, gitignore, delete useless tests deps

--- app/src/main/java/com/membattle/presentation/main/fragment/profile/ProfileFragmentView.java
+++ app/src/main/java/com/membattle/presentation/main/fragment/profile/ProfileFragmentView.java
@@ -1,7 +1,6 @@
 package com.membattle.presentation.main.fragment.profile;
 
 import com.arellomobile.mvp.MvpView;
-import com.membattle.presentation.main.fragment.profile.recycler.StatMode;
 
 import java.util.ArrayList;
 
